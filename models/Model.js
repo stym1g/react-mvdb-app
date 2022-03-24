df4e1d5566cc55b6c7aa965b3d0a7b528120358e
stym1g <satyamg650@gmail.com>
2022-03-24 23:46:27 +0530

    server 1st commit

---
+++ models/Model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const Schema = mongoose.Schema;
+const jwt = require('jsonwebtoken');
+const moment = require("moment");
+const favoriteSchema = mongoose.Schema({
+    userFrom: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    movieId: {
+        type: String
+    },
+    movieTitle: {
+        type: String
+    },
+    movieImage: {
+        type: String
+    },
+    movieRunTime: {
+        type:String
+    }
+});
+
+const Favorite = mongoose.model('Favorite', favoriteSchema);
+
+module.exports = { Favorite }
